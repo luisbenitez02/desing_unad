6d526d78667af9beb75af5ddcf113ac0b10fd344
Luis Benitez <luisbenitez02@gmail.com>
2014-09-30 21:31:31 -0500

    Agregado Codigo javascript
    Se agrego Javascript, al dar click sobre los botones tienda y carrito se
    despliega una alerta, se arreglo al grilla de la tienda y banner a la
    pagina principal

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){//Iniciamos el script solo cuando la pagia carge completamente
+$('.inter').click(function(){//cuando se de un click sobre los elementos con la clase inter (lupa y carrito)
+	alert(":( Esto es incomodo... Aun no tenemos lista esta caracteristica");//enviamos un mensaje
+});
+});
